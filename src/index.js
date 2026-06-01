83b07f963a3878a282aa0892b9194b3fe7a15f77
DarkSkillz <githubskillz@gmail.com>
2026-06-01 15:10:18 +0100

    Prepare elements in HTML

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./styles.css"; 
 import { Project, Task, Librarian } from "../classes.js";
 
 const project0 = new Project("Project0")
@@ -12,7 +13,5 @@ const projects = Librarian.getAllProjects()
 projects[0].addTask(new Task("Task0","28th May 2026", "High","Not Completed", projects[0]._name))
 projects[1].addTask(new Task("Task1","29th May 2026", "Medium","Completed", projects[1]._name))
 projects[2].addTask(new Task("Task2","30th May 2026", "Low","Not Completed", projects[2]._name))
-
-console.log(projects);
 
 Librarian.deleteProject(project1)
